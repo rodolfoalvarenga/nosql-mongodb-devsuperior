25fbbd80e5abcda39228cbcc8d547db6f798827c
RodolfoAlvarenga <rodolfo.l.alvarenga@gmail.com>
2021-02-16 17:40:10 -0300

    Author, Comment, Post

--- src/main/java/com/devsuperior/workshopmongo/config/TestConfig.java
+++ src/main/java/com/devsuperior/workshopmongo/config/TestConfig.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.devsuperior.workshopmongo.models.entities.User;
+import com.devsuperior.workshopmongo.repositories.PostRepository;
 import com.devsuperior.workshopmongo.repositories.UserRepository;
 
 @Configuration
@@ -18,9 +19,13 @@ public class TestConfig {
 	@Autowired
 	private UserRepository userRepository;
 	
+	@Autowired
+	private PostRepository postRepository;
+	
 	@PostConstruct
 	public void init() {
 		userRepository.deleteAll();
+		postRepository.deleteAll();
 		
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
