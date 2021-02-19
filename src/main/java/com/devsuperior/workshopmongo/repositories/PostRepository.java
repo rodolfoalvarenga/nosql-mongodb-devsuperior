dde7562e404b9998555603b8fbc6a235e2a6ea9f
RodolfoAlvarenga <rodolfo.l.alvarenga@gmail.com>
2021-02-19 10:11:59 -0300

    GET Post by title, query operators

--- src/main/java/com/devsuperior/workshopmongo/repositories/PostRepository.java
+++ src/main/java/com/devsuperior/workshopmongo/repositories/PostRepository.java
@@ -3,6 +3,7 @@ package com.devsuperior.workshopmongo.repositories;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.devsuperior.workshopmongo.models.entities.Post;
@@ -10,5 +11,8 @@ import com.devsuperior.workshopmongo.models.entities.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
+	
 	List<Post> findByTitleContainingIgnoreCase(String text);
 }
