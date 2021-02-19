799e760bc1486d1cb6292309e5b0e9c0c6a4e0fe
RodolfoAlvarenga <rodolfo.l.alvarenga@gmail.com>
2021-02-19 09:21:14 -0300

    GET Post by id

---
+++ src/main/java/com/devsuperior/workshopmongo/services/PostService.java
@@ -0,0 +1,28 @@
+package com.devsuperior.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.devsuperior.workshopmongo.models.dto.PostDTO;
+import com.devsuperior.workshopmongo.models.entities.Post;
+import com.devsuperior.workshopmongo.repositories.PostRepository;
+import com.devsuperior.workshopmongo.services.exceptions.ResourceNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repository;
+	
+	public PostDTO findById(String id) {
+		Post entity = getEntityById(id);
+		return new PostDTO(entity);
+	}
+	
+	private Post getEntityById(String id) {
+		Optional<Post> result = repository.findById(id);
+		return result.orElseThrow(() -> new ResourceNotFoundException("Objeto não encontrado"));
+	}
+}
